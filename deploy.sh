dee40f5d4171bf4fd343b950f90818a66cb0769e
lzc.java@icloud.com <Lzc@123.com>
2020-06-06 23:25:21 +0800

    update myapp

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@
 scp -c chacha20-poly1305@openssh.com  -o "Compression yes" build/* pi@raspberrypi:/home/pi/work/build/
 
 
-ssh pi@raspberrypi "pwd; echo '你好，alioo,restart homeserver'; /home/pi/work/build/stop.sh; /home/pi/work/build/start.sh"
+ssh pi@raspberrypi "echo 'restart homeserver start,check homeserver process...';ps aux|grep homeserver; /home/pi/work/build/stop.sh;/home/pi/work/build/start.sh;echo 'restart homeserver end,check homeserver process...';ps aux|grep homeserver"
+
+
--- shell/start.sh
+++ shell/start.sh
@@ -5,7 +5,7 @@ path_base=`dirname $0`
 
 #java -jar $path_base/homeserver-0.0.1-SNAPSHOT.jar &
 cmd="java -jar $path_base/homeserver-0.0.1-SNAPSHOT.jar "
-nohup $cmd >> output.log 2>&1  &
+nohup $cmd >> $path_base/output.log 2>&1  &
 
 # /home/pi/work/build/start.sh add to /etc/rc.local
 
