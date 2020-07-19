e02ef7d2eb52010631a410a48bd69361f0683c35
lzc.java@icloud.com <Lzc@123.com>
2020-07-19 18:01:51 +0800

    react basic study

---
+++ src/main/resources/static/precache-manifest.537515bf9ca5b3417284704679dfd4df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d35b4e263574d2a61e484f4b942070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5638bdb40602c235997",
+    "url": "/static/css/2.68c304fe.chunk.css"
+  },
+  {
+    "revision": "804d715391e5e225bf8a",
+    "url": "/static/css/main.f6f4de96.chunk.css"
+  },
+  {
+    "revision": "c5638bdb40602c235997",
+    "url": "/static/js/2.58183868.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/static/js/2.58183868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "804d715391e5e225bf8a",
+    "url": "/static/js/main.900f67b1.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
