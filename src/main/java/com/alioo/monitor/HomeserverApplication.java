e940b7e5cc26be9a546e6f943c5397b03d9af672
lzc.java@icloud.com <Lzc@123.com>
2020-07-28 22:37:13 +0800

    开启tomcat日志

--- src/main/java/com/alioo/monitor/HomeserverApplication.java
+++ src/main/java/com/alioo/monitor/HomeserverApplication.java
@@ -1,9 +1,6 @@
 package com.alioo.monitor;
 
 import com.alioo.monitor.homeserver.NettyServer;
-import com.alioo.monitor.homeserver.ServerHandler;
-import com.alioo.monitor.router.FlowStatisticService;
-import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.net.InetSocketAddress;
-import java.util.Random;
 
 @SpringBootApplication
 public class HomeserverApplication implements CommandLineRunner {
@@ -29,9 +25,8 @@ public class HomeserverApplication implements CommandLineRunner {
     @Autowired
     private NettyServer server;
 
-    @Autowired
-    private FlowStatisticService flowStatisticService;
-
+//    @Autowired
+//    private FlowStatisticService flowStatisticService;
 
     public static void main(String[] args) {
         SpringApplication.run(HomeserverApplication.class, args);
@@ -39,36 +34,35 @@ public class HomeserverApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        new Thread(() -> {
+//        new Thread(() -> {
+//
+//            while (true) {
+//                logger.info("ServerHandler.set=" + ServerHandler.set);
+//
+//                String ret = "aabbcc" + new Random().nextInt(100) + "\n";
+//
+//
+//                for (ChannelHandlerContext ctx : ServerHandler.set) {
+//
+//                    ctx.write(ret);
+//                    ctx.flush();
+//                }
+//
+//                try {
+//                    Thread.sleep(60000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//
+//        }).start();
 
-            while (true) {
-                logger.info("ServerHandler.set=" + ServerHandler.set);
-
-                String ret = "aabbcc" + new Random().nextInt(100) + "\n";
-
-
-                for (ChannelHandlerContext ctx : ServerHandler.set) {
-
-                    ctx.write(ret);
-                    ctx.flush();
-                }
-
-                try {
-                    Thread.sleep(60000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-
-        }).start();
-
-        flowStatisticService.getList();
+//        flowStatisticService.getList();
 
         InetSocketAddress address = new InetSocketAddress(url, port);
-        System.out.println("run  .... . ... " + url);
+        System.out.println("run  .... . ... " + url+":"+port);
         server.start(address);
-
 
     }
 
