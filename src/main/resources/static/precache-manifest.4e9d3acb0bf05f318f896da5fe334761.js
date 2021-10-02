0da52277318406ce7cb9a0e5d9fb2fcef92fbfe0
lzc.java@icloud.com <Lzc@123.com>
2021-10-02 23:00:44 +0800

    add chart

---
+++ src/main/resources/static/precache-manifest.4e9d3acb0bf05f318f896da5fe334761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ce5117e4dbbe075da6c14a416ae1f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "892ad0d7adb7714ec92c",
+    "url": "/static/css/2.20af6506.chunk.css"
+  },
+  {
+    "revision": "496fd72522de4f6d7c80",
+    "url": "/static/css/main.f6f4de96.chunk.css"
+  },
+  {
+    "revision": "892ad0d7adb7714ec92c",
+    "url": "/static/js/2.d5f187d2.chunk.js"
+  },
+  {
+    "revision": "a44a3522db34fab7523f67fbb8d57019",
+    "url": "/static/js/2.d5f187d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496fd72522de4f6d7c80",
+    "url": "/static/js/main.188a0fb2.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
