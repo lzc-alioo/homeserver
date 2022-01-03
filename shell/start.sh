1bb5996ba12f0603c89306597b66335b7f204605
lzc.java@icloud.com <Lzc@123.com>
2022-01-03 16:12:09 +0800

    hlold

--- shell/start.sh
+++ shell/start.sh
@@ -3,25 +3,27 @@
 
 PATH_BASE=`dirname $0`
 RUN_APP=homeserver-0.0.1-SNAPSHOT.jar
-LOG_FILE=logs/`date +%Y%m%d%H%M%S`.log
+LOG_PATH=$PATH_BASE/logs
+LOG_FILE=$LOG_PATH/`date +%Y%m%d%H%M%S`.log
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") $RUN_APP start... you can see log:$LOG_FILE"
+mkdir -p $LOG_PATH
+echo "$(date "+%Y-%m-%d %H:%M:%S") $RUN_APP start... user_dir:$PATH_BASE, and you can see log:$LOG_FILE"
 
 cd $PATH_BASE
 sudo chown -R pi:pi $PATH_BASE/*
 
 #java -jar $PATH_BASE/homeserver-0.0.1-SNAPSHOT.jar &
 cmd="java -jar $PATH_BASE/homeserver-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod"
-nohup $cmd >> $PATH_BASE/$LOG_FILE 2>&1  &
+nohup $cmd >> $LOG_FILE 2>&1  &
 
 # 查询日志检测java程序是否启动成功
 echo "$(date "+%Y-%m-%d %H:%M:%S") checking if started ..."
 
-while [ -f $PATH_BASE/$LOG_FILE ]
+while [ -f $LOG_FILE ]
 do
     current=`date +%Y-%m-%d\ %H:%M`
-    echo "cmd:grep \"$current\" $PATH_BASE/$LOG_FILE | grep \"Started Application\""
-    result=`grep "$current" $PATH_BASE/$LOG_FILE | grep "Started Application"`
+    echo "cmd:grep \"$current\" $LOG_FILE | grep \"Started Application\""
+    result=`grep "$current" $LOG_FILE | grep "Started Application"`
 
     if [ "x$result" != "x" ]
     then
