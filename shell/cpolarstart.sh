5904e9e8c13795db1eb6ea577f8eb0c93c395f2b
lzc.java@icloud.com <Lzc@123.com>
2022-01-08 13:59:45 +0800

    optimize sh

--- shell/cpolarstart.sh
+++ shell/cpolarstart.sh
@@ -3,13 +3,13 @@
 
 PATH_BASE=$(cd $(dirname $0); pwd)
 RUN_APP=cpolar
-LOG_FILE=logs/`date +%Y%m%d%H%M%S`.log
+LOG_FILE=$PATH_BASE/logs/`date +%Y%m%d%H%M%S`.log
 
 
 echo "$(date "+%Y-%m-%d %H:%M:%S") $RUN_APP start... you can see log:$LOG_FILE"
 
 cd $PATH_BASE
-sudo chown -R pi:pi $PATH_BASE/*
+#sudo chown -R pi:pi $PATH_BASE/*
 
-cmd="su - pi -c \"$PATH_BASE/cpolar http 8081 \""
-nohup $cmd >> $PATH_BASE/$LOG_FILE -log=stdout 2>&1   &
+cmd="$PATH_BASE/cpolar http 8081 -log=stdout"
+nohup $cmd >> $LOG_FILE 2>&1   &
