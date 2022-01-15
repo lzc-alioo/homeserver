56e4dc565d20de7c4449182afa6eab53b7951ee4
lzc.java@icloud.com <Lzc@123.com>
2022-01-15 20:35:36 +0800

    optimize sh

--- shell/cpolarrestart.sh
+++ shell/cpolarrestart.sh
@@ -2,13 +2,18 @@
 
 RUN_APP=cpolar
 
-echo 'restart $RUN_APP start,check cpolar process...';
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "$(date "+%Y-%m-%d %H:%M:%S") restart $RUN_APP start,check cpolar process...";
 ps aux|grep $RUN_APP
 
 /home/pi/work/cpolar/cpolarstop.sh;
 /home/pi/work/cpolar/cpolarstart.sh;
 
-echo 'restart $$RUN_APP end,check cpolar process...';
+echo '$(date "+%Y-%m-%d %H:%M:%S") restart $$RUN_APP end,check cpolar process...';
 ps aux|grep $RUN_APP
 
 
--- shell/cpolarstop.sh
+++ shell/cpolarstop.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+#注意事项
+#stop脚本中，获取需要停止的进程id，要检查下不要误杀
+
+
 RUN_APP=cpolar
 
 
-echo "开始执行cpolarstop.sh..."
+echo "$(date "+%Y-%m-%d %H:%M:%S") 开始执行cpolarstop.sh..."
 
-pid=$(ps aux | grep $RUN_APP | grep -v grep | grep -v cpolarstop | awk -F ' ' '{print $2}')
+pid=$(ps aux | grep $RUN_APP | grep -v grep | grep -v cpolarstop  | grep -v cpolarrestart.sh | awk -F ' ' '{print $2}')
 
 if [ "" != "$pid" ]; then
     sudo kill -9 $pid
-    echo "执行cpolarstop.sh时计划kill掉进程pid:$pid,执行结果:$?"
+    echo "$(date "+%Y-%m-%d %H:%M:%S") 执行cpolarstop.sh时计划kill掉进程pid:$pid,执行结果:$?"
 else
-    echo "执行cpolarstop.sh时发现进程不存在，直接跳过"
+    echo "$(date "+%Y-%m-%d %H:%M:%S") 执行cpolarstop.sh时发现进程不存在，直接跳过"
 fi
 
 
